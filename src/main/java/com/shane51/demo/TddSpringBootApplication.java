f6c71a6d12a3af706509ec7a8bf87377267de730
shane51 <suse51@163.com>
2022-03-29 19:51:16 +0800

    init spring boot start project

---
+++ src/main/java/com/shane51/demo/TddSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.shane51.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TddSpringBootApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TddSpringBootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shane51/demo/TddSpringBootApplicationTest.java
@@ -0,0 +1,15 @@
+package com.shane51.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TddSpringBootApplicationTest {
+    @Test
+    void contextLoad() {
+
+    }
+
+}
