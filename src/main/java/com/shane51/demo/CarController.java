7b99dc9b8f40ea8169f24117a8d0faecb98d75ad
shane51 <suse51@163.com>
2022-04-01 22:18:06 +0800

    pass status is ok assertion

--- src/main/java/com/shane51/demo/CarController.java
+++ src/main/java/com/shane51/demo/CarController.java
@@ -1,4 +1,14 @@
 package com.shane51.demo;
 
+import com.shane51.demo.domain.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
+    @GetMapping("/cars/{name}")
+    private Car getCar(@PathVariable String name) {
+        return null;
+    }
 }
