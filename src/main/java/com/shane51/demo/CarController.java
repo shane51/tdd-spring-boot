fbbe954c521668fb269a2a9f51b5322d6c383892
shane51 <suse51@163.com>
2022-04-01 22:40:38 +0800

    pass CarNotFound test with ExceptionHandler

--- src/main/java/com/shane51/demo/CarController.java
+++ src/main/java/com/shane51/demo/CarController.java
@@ -1,9 +1,9 @@
 package com.shane51.demo;
 
 import com.shane51.demo.domain.Car;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.shane51.demo.exception.CarNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CarController {
@@ -18,4 +18,8 @@ public class CarController {
     private Car getCar(@PathVariable String name) {
         return carService.getCarDetails(name);
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private void carNotFoundHandler(CarNotFoundException ex) {}
 }
