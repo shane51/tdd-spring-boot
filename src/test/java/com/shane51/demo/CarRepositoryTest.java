3240d99867587884ebe8b0432ce8510b8bf09a5d
shane51 <suse51@163.com>
2022-04-04 23:26:42 +0800

    pass repository unit test

--- src/test/java/com/shane51/demo/CarRepositoryTest.java
+++ src/test/java/com/shane51/demo/CarRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.shane51.demo;
 
 import com.shane51.demo.domain.Car;
+import com.shane51.demo.domain.CarRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,8 +17,14 @@ class CarRepositoryTest {
     @Autowired
     private CarRepository carRepository;
 
+    @Autowired
+    private TestEntityManager testEntityManager;
+
     @Test
     void shouldReturnCarDetails() {
+        Car savedCar = new Car("prius", "hybrid");
+        testEntityManager.persistAndFlush(savedCar);
+
         Car car = carRepository.findByName("prius");
         assertThat(car.getName()).isEqualTo("prius");
         assertThat(car.getType()).isEqualTo("hybrid");
