a51dfafbe35a4842a585bf50ebecf4dcca72c1a2
shane51 <suse51@163.com>
2022-04-05 21:58:58 +0800

    add failed test for caching

---
+++ src/test/java/com/shane51/demo/CachingTest.java
@@ -0,0 +1,34 @@
+package com.shane51.demo;
+
+import com.shane51.demo.domain.Car;
+import com.shane51.demo.domain.CarRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureTestDatabase
+public class CachingTest {
+    @Autowired
+    private CarService carService;
+    @Autowired
+    private CarRepository carRepository;
+    @Test
+    public void caching() {
+        given(carRepository.findByName("prius")).willReturn(new Car("prius", "hybrid"));
+
+        carService.getCarDetails("prius");
+        carService.getCarDetails("prius");
+
+        verify(carRepository, times(1)).findByName("prius");
+
+    }
+}
